dc32aad8fafb7581630a90d0fd36ed4e308fbcaf
Gastón Martínez <gastongmartinez@gmail.com>
2023-08-25 13:12:17 -0300

    Agrega Devuan

---
+++ Devuan/0_setup.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Validacion del usuario ejecutando el script
+R_USER=$(id -u)
+if [ "$R_USER" -ne 0 ];
+then
+    echo -e "\nDebe ejecutar este script como root.\n"
+    exit 1
+fi
+if [ "$LOGNAME" != "root" ]; then
+    echo -e "\nEs necesario iniciar la sesión como root (su -).\n"
+    exit 1
+fi
+
+USER=$(grep "1000" /etc/passwd | awk -F : '{ print $1 }')
+usermod -aG sudo "$USER"
+
+# Apt config
+cp /etc/apt/sources.list /etc/apt/sources.list.ORIGINAL
+sed -i "2d;/deb cdrom/d" "/etc/apt/sources.list"
+sed -i "s/main/main contrib non-free/g" "/etc/apt/sources.list"
+apt install apt-transport-https -y
+apt install curl -y
+apt install nala -y
+
+# Ajuste Swappiness
+echo -e "vm.swappiness=10\n" >> /etc/sysctl.d/90-sysctl.conf
+
+# VSCODE Repo
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+# Brave
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Librewolf
+distro=$(if echo " una vanessa focal jammy bullseye vera uma" | grep -q " $(lsb_release -sc) "; then lsb_release -sc; else echo focal; fi)
+wget -O- https://deb.librewolf.net/keyring.gpg | gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+
+# Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+
+apt update
+
+# Limpieza de paquetes
+apt remove firefox-esr -y
+apt remove libreoffice-base-core -y
+apt remove libreoffice-common -y
+apt remove libreoffice-style-colibre -y
+apt autoremove -y
+
+
+# Configuracion de servidores
+nala fetch
+
+# Nix
+sh <(curl -L https://nixos.org/nix/install) --daemon
---
+++ Devuan/1_install.sh
@@ -0,0 +1,206 @@
+#!/usr/bin/env bash
+
+# Validacion del usuario ejecutando el script
+R_USER=$(id -u)
+if [ "$R_USER" -ne 0 ];
+then
+    echo -e "\nDebe ejecutar este script como root.\n"
+    exit 1
+fi
+
+USER=$(grep "1000" /etc/passwd | awk -F : '{ print $1 }')
+
+############################### Apps Generales ################################
+PAQUETES=(
+    #### Powermanagement ####
+    'tlp'
+    'tlp-rdw'
+    'powertop'
+
+    #### WEB ####
+    'google-chrome-stable'
+    'librewolf'
+    'brave-browser'
+    'thunderbird'
+    'remmina'
+    'qbittorrent'
+
+    #### Shells ####
+    'zsh'
+    'zsh-autosuggestions'
+    'zsh-syntax-highlighting'
+    'dialog'
+    'autojump'
+    'shellcheck'
+
+    #### Archivos ####
+    'mc'
+    'vifm'
+    'stow'
+    'ripgrep'
+    'autofs'
+
+    #### Sistema ####
+    'flatpak'
+    'gnome-software-plugin-flatpak'
+    'tldr'
+    'lsd'
+    'unrar-free'
+    'alacritty'
+    'kitty'
+    'htop'
+    'bpytop'
+    'neofetch'
+    'lshw'
+    'lshw-gtk'
+    'powerline'
+    'emacs'
+    'flameshot'
+    'klavaro'
+    'fd-find'
+    'fzf'
+    'silversearcher-ag'
+    'qalculate-gtk'
+    'calibre'
+    'foliate'
+    'pandoc'
+    'dconf-editor'
+    'gettext'
+    'stacer'
+    'tmux'
+
+    #### Multimedia ####
+    'vlc'
+    'python3-vlc'
+    'mpv'
+    'lame'
+    'ffmpeg'
+    'libavcodec-extra'
+
+    #### Juegos ####
+    'chromium-bsu'
+    'retroarch'
+
+    #### Redes ####
+    'nmap'
+    'wireshark'
+    'wireshark-gtk'
+    'firewall-applet'
+
+    #### Diseño ####
+    'gimp'
+    'inkscape'
+    #'krita'
+    #'blender'
+
+    #### DEV ####
+    'clang'
+    'cmake'
+    'meson'
+    'pipenv'
+    #'python3-spyder'
+    'python3-pip'
+    'pipx'
+    'filezilla'
+    'sbcl'
+    'golang'
+    'lldb'
+    'code'
+    'tidy'
+    'nodejs'
+    'npm'
+    'yarnpkg'
+
+    #### Fuentes ####
+    'fonts-terminus'
+    'fonts-font-awesome'
+    'fonts-roboto'
+    'fonts-firacode'
+    'cabextract'
+    'fonts-crosextra-caladea'
+    'fontforge'
+    'ttf-mscorefonts-installer'
+
+    #### Bases de datos ####
+    'postgresql'
+    'postgis'
+    'sqlite3'
+    'sqlite3-tools'
+    'sqlitebrowser'
+    'mariadb-server'
+
+    #### Cockpit ####
+    'cockpit'
+    'cockpit-sosreport'
+    'cockpit-machines'
+    'cockpit-podman'
+
+    #### Virtualizacion ####
+    'virt-manager'
+    'ebtables'
+    'bridge-utils'
+
+    #### Window Managers ####
+    'awesome'
+    'nitrogen'
+    'feh'
+    'picom'
+    'lxappearance'
+    'jgmenu'
+    'i3lock'
+    'i3lock-fancy'
+    'sway'
+    'sway-notification-center'
+    'swayidle'
+    'swayimg'
+    'swaykbdd'
+    'swaylock'
+    'grimshot'
+    'waybar'
+    'wofi'
+    'wlr-randr'
+    'wlogout'
+    'pavucontrol'
+)
+
+for PAQ in "${PAQUETES[@]}"; do
+    nala install "$PAQ" -y
+done
+
+wget https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.deb
+wget https://github.com/Ulauncher/Ulauncher/releases/download/5.15.2/ulauncher_5.15.2_all.deb
+apt install ./amazon-corretto-17-x64-linux-jdk.deb -y
+apt install ./ulauncher_5.15.2_all.deb -y
+###############################################################################
+
+################################ Wallpapers #####################################
+echo -e "\nInstalando wallpapers..."
+git clone https://github.com/gastongmartinez/wallpapers.git
+mv -f wallpapers/ "/usr/share/backgrounds/"
+#################################################################################
+
+#################################################################################
+sed -i "s/Name=awesome/Name=Awesome/g" "/usr/share/xsessions/awesome.desktop"
+
+usermod -aG libvirt "$USER"
+usermod -aG kvm "$USER"
+
+systemctl enable --now cockpit.socket
+firewall-cmd --add-service=cockpit
+firewall-cmd --add-service=cockpit --permanent
+#################################################################################
+
+############################### GRUB ############################################
+#git clone https://github.com/vinceliuice/grub2-themes.git
+#cd grub2-themes || return
+#./install.sh
+#cd .. || return
+#################################################################################
+
+rm -rf grub2-themes
+rm amazon-corretto-17-x64-linux-jdk.deb
+rm ulauncher_5.15.2_all.deb
+
+sleep 2
+
+reboot
