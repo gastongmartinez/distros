a65fa957f6a16ca6af8b12165e647844477ebafe
Gastón Martínez <gastongmartinez@gmail.com>
2023-08-25 13:11:55 -0300

    Agrega Debian

---
+++ Debian/2_gnome_extensions.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+R_USER=$(id -u)
+if [ "$R_USER" -eq 0 ];
+then
+   echo "Este script debe usarse con un usuario regular."
+   echo "Saliendo..."
+   exit 1
+fi
+
+if [ -z "$DISPLAY" ];
+then
+    echo -e "Debe ejecutarse dentro del entorno grafico.\n"
+    echo "Saliendo..."
+    exit 2
+fi
+
+DIRE=$(pwd)
+# Arc Menu
+git clone https://gitlab.com/arcmenu/ArcMenu.git
+cd ArcMenu || return
+make install
+cd "$DIRE" || return
+rm -rf ArcMenu
+
+# Vitals
+git clone https://github.com/corecoding/Vitals.git ~/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com
+
+# Systemd Manager
+git clone https://github.com/hardpixel/systemd-manager.git
+cd systemd-manager || return
+mv ./systemd-manager@hardpixel.eu ~/.local/share/gnome-shell/extensions/systemd-manager@hardpixel.eu
+cd "$DIRE" || return
+rm -rf systemd-manager
+
+# Removable Drive
+git clone https://gitlab.gnome.org/GNOME/gnome-shell-extensions.git
+cd gnome-shell-extensions || return
+git checkout gnome-43
+meson build
+ninja -C build install
+cd "$DIRE" || return
+rm -rf gnome-shell-extensions
+
+# Blur my shell
+git clone https://github.com/aunetx/blur-my-shell
+cd blur-my-shell || return
+make install
+cd "$DIRE" || return
+rm -rf blur-my-shell
+
+# No Overview
+git clone https://github.com/fthx/no-overview.git
+mv no-overview ~/.local/share/gnome-shell/extensions/no-overview@fthx
+
+# Aylur widgets
+git clone https://github.com/Aylur/gnome-extensions.git
+cd gnome-extensions || return
+mv ./widgets@aylur ~/.local/share/gnome-shell/extensions/widgets@aylur
+cd "$DIRE" || return
+rm -rf gnome-extensions
+
+sleep 2
+
+reboot
---
+++ Debian/3_user_apps.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+R_USER=$(id -u)
+if [ "$R_USER" -eq 0 ]; then
+   echo "Este script debe usarse con un usuario regular."
+   echo "Saliendo..."
+   exit 1
+fi
+
+if [ ! -d ~/Apps ]; then
+    mkdir ~/Apps
+fi
+
+# NerdFonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d ~/.local/share/fonts
+fc-cache -f -v
+rm JetBrainsMono.zip
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+if [ ! -d ~/.local/bin ]; then
+    mkdir -p ~/.local/bin
+fi
+curl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+chmod +x ~/.local/bin/rust-analyzer
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Flatpak
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak --user install flathub fr.handbrake.ghb -y
+flatpak --user install flathub md.obsidian.Obsidian -y
+flatpak --user install flathub com.mattjakeman.ExtensionManager -y
+flatpak --user install flathub io.github.shiftey.Desktop -y
+flatpak --user install flathub net.ankiweb.Anki -y
+flatpak --user install flathub com.github.marktext.marktext -y
+flatpak --user install flathub com.rafaelmardojai.Blanket -y
+flatpak --user install flathub com.github.tchx84.Flatseal -y
+flatpak --user install flathub com.github.neithern.g4music -y
+flatpak --user install flathub com.axosoft.GitKraken -y
+flatpak --user install flathub org.mozilla.firefox -y
+flatpak --user install flathub org.libreoffice.LibreOffice -y
+
+# Doom Emacs
+read -rp "Instalar Doom Emacs? (S/N): " DOOM
+if [[ $DOOM =~ ^[Ss]$ ]]; then
+    if [ -d ~/.emacs.d ]; then
+        rm -Rf ~/.emacs.d
+    fi
+    go install github.com/fatih/gomodifytags@latest
+    go install github.com/cweill/gotests/...@latest
+    go install github.com/x-motemen/gore/cmd/gore@latest
+    go install golang.org/x/tools/cmd/guru@latest
+    pipx install nose
+    git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+    ~/.emacs.d/bin/doom install
+    sleep 5
+    rm -rf ~/.doom.d
+fi
+
+# Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+# Anaconda
+read -rp "Instalar Anaconda3? (S/N): " ANA
+if [[ $ANA =~ ^[Ss]$ ]]; then
+    wget https://repo.anaconda.com/archive/Anaconda3-2023.07-1-Linux-x86_64.sh
+    chmod +x Anaconda3-2023.07-1-Linux-x86_64.sh
+    ./Anaconda3-2023.07-1-Linux-x86_64.sh
+fi
+
+# Bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+sed -i 's/"font"/"powerline"/g' "$HOME/.bashrc"
+
+# ZSH
+if [ ! -d ~/.local/share/zsh ]; then
+    mkdir ~/.local/share/zsh
+fi
+touch ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/zsh/powerlevel10k
+chsh -s /usr/bin/zsh
+
+# Nix
+export NIXPKGS_ALLOW_UNFREE=1
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.helix
+nix-env -iA nixpkgs.lazygit
+nix-env -iA nixpkgs.pgadmin4
+#nix-env -iA nixpkgs.mysql80
+nix-env -iA nixpkgs.mysql-workbench
+nix-env -iA nixpkgs.sqlite-analyzer
+nix-env -iA nixpkgs.corectrl
+
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+pipx install black
+pipx install 'python-lsp-server[all]'
+pipx install pyright
+pipx install yamllint
+pipx install autopep8
+cargo install taplo-cli --locked
+cargo install stylua
+sudo npm install -g neovim prettier bash-language-server vscode-langservers-extracted emmet-ls typescript typescript-language-server yaml-language-server live-server markdownlint markdownlint-cli dockerfile-language-server-nodejs stylelint js-beautify
+
+jgmenu_run init --theme=archlabs_1803
+
+sleep 5
+
+reboot
+
