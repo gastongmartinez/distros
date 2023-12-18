bb0c19f64e4ee0f86e1ecc57664b69bd109f5631
Gastón Martínez <gastongmartinez@gmail.com>
2023-12-18 13:58:39 -0300

    Limpieza

--- openSUSE/Tumbleweed/2_user_apps.sh
+++ openSUSE/Tumbleweed/2_user_apps.sh
@@ -57,6 +57,7 @@ if [[ $ANA =~ ^[Ss]$ ]]; then
     wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh
     chmod +x Anaconda3-2023.09-0-Linux-x86_64.sh
     ./Anaconda3-2023.09-0-Linux-x86_64.sh
+    rm Anaconda3-2023.09-0-Linux-x86_64.sh
 fi
 
 # Bash
