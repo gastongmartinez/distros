d38c03465d3a84b29c72442ab8e7999f773f590a
Gastón Martínez <gastongmartinez@gmail.com>
2024-11-09 22:03:25 -0300

    Actualiza instalacion ATUIN

--- FedoraKDE/2_user_apps.sh
+++ FedoraKDE/2_user_apps.sh
@@ -93,9 +93,10 @@ wget https://github.com/artempyanykh/marksman/releases/download/2023-12-09/marks
 mv marksman-linux-x64 marksman
 chmod +x marksman
 mv marksman "$HOME/.local/bin/"
-cargo install atuin
 
-jgmenu_run init --theme=archlabs_1803
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+
+
 
 sleep 5
 
