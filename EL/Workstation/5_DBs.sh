febe09196d3dc58df9fc74619b58907ac70fe254
Gastón Martínez <gastongmartinez@gmail.com>
2024-11-19 19:25:58 -0300

    Agrega PostgreSQL

--- EL/Workstation/5_DBs.sh
+++ EL/Workstation/5_DBs.sh
@@ -24,4 +24,24 @@ if [[ $MYSQL =~ ^[Ss]$ ]]; then
     dnf install -y mysql-community-server
 
     dnf install -y https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-8.0.40-1.el9.x86_64.rpm
+
+    mysql_secure_installation
+fi
+
+# PostgreSQL
+read -rp "Instalar PostgreSQL? (S/N): " PSQL
+if [[ $PSQL =~ ^[Ss]$ ]]; then
+    dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
+    # Disable the built-in PostgreSQL module:
+    dnf -qy module disable postgresql
+
+    dnf install -y postgresql17-server
+
+    /usr/pgsql-17/bin/postgresql-17-setup initdb
+    systemctl enable --now postgresql-17
+
+    # PGAdmin4
+    rpm -i https://ftp.postgresql.org/pub/pgadmin/pgadmin4/yum/pgadmin4-redhat-repo-2-1.noarch.rpm
+    dnf install pgadmin4
 fi
