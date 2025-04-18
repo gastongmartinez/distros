69fe8c6f58d4f9c4d0cda4cb8e0d4b57142dddb8
Gastón Martínez <gastongmartinez@gmail.com>
2025-04-17 22:43:20 -0300

    Elimina extensiones

--- Ubuntu/2_gnome_extensions.sh
+++ Ubuntu/2_gnome_extensions.sh
@@ -21,21 +21,12 @@ make install
 cd "$DIRE" || return
 rm -rf ArcMenu
 
-# Dash to Dock
-git clone https://github.com/micheleg/dash-to-dock.git
-make -C dash-to-dock install
-rm -rf dash-to-dock
-
 # Blur my shell
 git clone https://github.com/aunetx/blur-my-shell
 cd blur-my-shell || return
 make install
 cd "$DIRE" || return
 rm -rf blur-my-shell
-
-# No Overview
-git clone https://github.com/fthx/no-overview.git
-mv no-overview ~/.local/share/gnome-shell/extensions/no-overview@fthx
 
 # Vitals
 git clone https://github.com/corecoding/Vitals.git ~/.local/share/gnome-shell/extensions/Vitals@CoreCoding.com
@@ -46,21 +37,6 @@ cd systemd-manager || return
 mv ./systemd-manager@hardpixel.eu ~/.local/share/gnome-shell/extensions/systemd-manager@hardpixel.eu
 cd "$DIRE" || return
 rm -rf systemd-manager
-
-# Tiling assistant
-git clone https://github.com/Leleat/Tiling-Assistant.git
-cd Tiling-Assistant/scripts || return
-chmod +x build.sh
-./build.sh -i
-cd "$DIRE" || return
-rm -rf Tiling-Assistant
-
-# Aylur widgets
-git clone https://github.com/Aylur/gnome-extensions.git
-cd gnome-extensions || return
-mv ./widgets@aylur ~/.local/share/gnome-shell/extensions/widgets@aylur
-cd "$DIRE" || return
-rm -rf gnome-extensions
 
 # Caffeine
 git clone https://github.com/eonpatapon/gnome-shell-extension-caffeine.git
