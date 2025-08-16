eb23c70c919bd6e30c7d75dbd14eb795237f0847
Gastón Martínez <gastongmartinez@gmail.com>
2025-08-15 21:34:49 -0300

    Agrega Lazyvim

--- EL/OL/2_user_apps.sh
+++ EL/OL/2_user_apps.sh
@@ -13,3 +13,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 # Bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 sed -i 's/"font"/"powerline"/g' "$HOME/.bashrc"
+
+# Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+tar -C "$HOME/.local" -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
+echo -e "\nexport PATH=\"$HOME/.local/nvim-linux-x86_64/bin:$PATH\"\n" >> "$HOME/.bashrc"
+
+# Lazyvim
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
